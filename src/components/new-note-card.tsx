281f13ceab38096b141d68db303e953560439fbf
Francisco Montalvao <f.montalvao@outlook.com>
2024-02-09 19:29:14 -0300

    terminado aplicacao de notas reactJcS

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog"
-import { set } from "date-fns"
-import { tr } from "date-fns/locale"
+/* import { set } from "date-fns"
+import { tr } from "date-fns/locale" */
 import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from "react"
 import { toast } from 'sonner'
